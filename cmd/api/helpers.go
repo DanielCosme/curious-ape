a5fea1c27de3dbb2e3a31850a7831e70b4869349
Daniel Cosme <cosmedaniel8@gmail.com>
2021-05-17 13:10:00 -0400

    Add further json parsing error handling

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -16,7 +17,13 @@ type envelope map[string]interface{}
 func (a *application) readJSON(rw http.ResponseWriter, r *http.Request,
 	data interface{}) error {
 
-	err := json.NewDecoder(r.Body).Decode(data)
+	maxBytes := 1_048_576 // 1MB
+	r.Body = http.MaxBytesReader(rw, r.Body, int64(maxBytes))
+
+	d := json.NewDecoder(r.Body)
+	d.DisallowUnknownFields()
+
+	err := d.Decode(data)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
@@ -28,8 +35,7 @@ func (a *application) readJSON(rw http.ResponseWriter, r *http.Request,
 				syntaxError.Offset)
 
 		// In some circumstances Decode() may also return an io.ErrUnexpectedEOF error
-		// for syntax errors in the JSON. So we check for this using errors.Is() and
-		// return a generic error message. There is an open issue regarding this at
+		// for syntax errors in the JSON. There is an open issue regarding this at
 		// https://github.com/golang/go/issues/25956.
 		case errors.Is(err, io.ErrUnexpectedEOF):
 			return errors.New("body contains badly-formed JSON")
@@ -45,14 +51,30 @@ func (a *application) readJSON(rw http.ResponseWriter, r *http.Request,
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
 
+		// there's an open issue at https://github.com/golang/go/issues/29035
+		// regarding turning this into a distinct error type in the future.
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+
+		// there is an open issue about turning
+		// this into a distinct error type at https://github.com/golang/go/issues/30715.
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
+
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
 
-		// For anything else, return the error message as-is.
 		default:
 			return err
 		}
 	}
+
+	err = d.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("the body cannot have more than one JSON value")
+	}
+
 	return nil
 }
 
