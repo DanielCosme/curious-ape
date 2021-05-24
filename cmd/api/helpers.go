08e005677b4e193a619d6fc8fe4d824e03609cda
Daniel Cosme <cosmedaniel8@gmail.com>
2021-05-24 10:45:40 -0400

    Allow for background jobs that if panic don't bring down server

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -93,18 +93,18 @@ func (a *application) writeJSON(rw http.ResponseWriter, status int, data envelop
 }
 
 // The background() helper accepts an arbitrary function as a parameter.
-// func (a *application) background(fn func()) {
-// 	// Launch a background goroutine.
-// 	go func() {
-// 		// Recover any panic.
-// 		defer func() {
-// 			if err := recover(); err != nil {
-// 				//a.logger.PrintError(fmt.Errorf("%s", err), nil)
-// 				a.logger.Println(err)
-// 			}
-// 		}()
-//
-// 		// Execute the arbitrary function that we passed as the parameter.
-// 		fn()
-// 	}()
-// }
+func (a *application) background(fn func()) {
+	// Launch a background goroutine.
+	go func() {
+		// Recover any panic.
+		defer func() {
+			if err := recover(); err != nil {
+				//a.logger.PrintError(fmt.Errorf("%s", err), nil)
+				a.logger.Println(err)
+			}
+		}()
+
+		// Execute the arbitrary function that we passed as the parameter.
+		fn()
+	}()
+}
