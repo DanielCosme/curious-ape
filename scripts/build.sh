09d884812d0920caf06cc28500e8c734ab57ada1
Daniel Cosme <cosmedaniel8@gmail.com>
2024-04-12 18:28:45 -0400

    Change versions for dev and staging environments

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 set -gx DOCKER_BUILDKIT 1
 
 if test -z $argv[1]
-  set -gx build_version "$(semver get alpha)"
+  set -gx build_version "$(semver get beta)"
 else
   set -gx build_version $argv[1]
 end
