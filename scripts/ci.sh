5aa8c591534008992832c4b77bf36dbd1f44f4ae
Daniel Cosme <cosmedaniel8@gmail.com>
2024-04-12 02:33:32 -0400

    Fix typo in CI script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -26,6 +26,10 @@ $cur_dir/test.sh; or exit 1
 
 $cur_dir/build.sh "$new_version-$(git rev-parse --short HEAD)"; or exit 1
 
+git tag $new_version
+git push origin $new_version || exit 1
+git push || or exit 1
+
 echo "$DOCKER_HUB_PASSWORD" | docker login -u $DOCKER_HUB_USER --password-stdin; or exit 1
 
 docker image tag curious-ape "danielcosme/curious-ape:latest"; or exit 1
@@ -35,8 +39,4 @@ docker image tag migrate-ape "danielcosme/migrate-ape:$new_version"; or exit 1
 docker push --all-tags danielcosme/curious-ape
 docker push --all-tags danielcosme/migrate-ape
 
-git tag $new_version
-git push $new_version
-git push
-
-echo "New version:" &new_version
+echo "New version:" $new_version
