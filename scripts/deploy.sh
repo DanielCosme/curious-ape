71de72f905958213b16685ab0e8ef3aec258a93f
Daniel Cosme <cosmedaniel8@gmail.com>
2024-05-12 14:12:07 -0400

    Update deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,15 @@
 
 if test "$RELEASE" = true
   echo "--- Pushing code to remote ---"
+  set current_branch (git branch --show-current)
 
   if not test $current_branch = "main"
     echo "git is NOT on the main branch"
     echo "current branch: " $current_branch
     exit 1
   end
-  git diff --exit-code; or echo "Working tree cannot be dirty"; exit 1
+
+  git diff --exit-code; or echo "Working tree cannot be dirty" and exit 1
   git push; or exit 1
   echo "--- Success ---"
   echo ""
