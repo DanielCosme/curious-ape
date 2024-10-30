9384e7b499c7f241d45bededec7db709db6b2f9d
Daniel Cosme <cosmedaniel8@gmail.com>
2024-10-30 17:26:04 -0500

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 set -gx DOCKER_BUILDKIT 1
 
 if test -z $argv[1]
-  set -gx build_version (semver get beta)
+  set -gx build_version (semver get beta);or exit
 else
   set -gx build_version $argv[1]
 end
@@ -12,9 +12,9 @@ docker build \
   --build-arg="APE_VERSION=$build_version" \
   --tag curious-ape \
   --target ape \
-  .
+  . ; or exit
 
 docker build \
   --tag migrate-ape \
   --target migrate \
-  .
+  . ; or exit
